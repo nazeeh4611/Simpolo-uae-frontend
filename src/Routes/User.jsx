a927c663cc3963796839cb8342765e9aa8760d0f
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-12-26 00:57:32 +0530

    1add

--- src/Routes/User.jsx
+++ src/Routes/User.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "../Pages/User/Home";
